af194bb8793c46c779b0b8e49dab9d94cef2320a
Gustavo da Costa <gustavocosta.nh@gmail.com>
2023-10-17 11:29:16 +0200

    Exception handler

--- WEB_SERVICE_WITH_SPRING/src/main/java/com/gustavocostabr/demo/services/UserService.java
+++ WEB_SERVICE_WITH_SPRING/src/main/java/com/gustavocostabr/demo/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.gustavocostabr.demo.entities.User;
 import com.gustavocostabr.demo.repositories.UserRepository;
+import com.gustavocostabr.demo.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
